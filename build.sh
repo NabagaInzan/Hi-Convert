e8e35709dceb328ed16c0c8a107b7635dc749d0e
NabagaInzan <ton-email@nabagainzan.com>
2025-02-13 12:03:28 +0000

    commit 4

--- build.sh
+++ build.sh
@@ -2,18 +2,9 @@
 # exit on error
 set -o errexit
 
-# Install Poppler
-chmod +x install_poppler.sh
-./install_poppler.sh
-
 # Install Python dependencies
 pip install -r requirements.txt
 
-# Create necessary directories
-mkdir -p staticfiles media uploads outputs
-
-# Collect static files
-python manage.py collectstatic --no-input
-
-# Run migrations
-python manage.py migrate
+# Create required directories
+mkdir -p uploads
+mkdir -p logs
--- pdf_converter/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('process-folder/', views.process_folder, name='process_folder'),
-    path('job-status/<int:job_id>', views.get_job_status, name='get_job_status'),
-    path('download-csv/<int:file_id>', views.download_csv, name='download_csv'),
-]
