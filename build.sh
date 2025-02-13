b3b2753736b2f041023b85a63397904a1885ae4a
NabagaInzan <ton-email@nabagainzan.com>
2025-02-13 12:09:24 +0000

    commit 4

--- build.sh
+++ build.sh
@@ -2,8 +2,15 @@
 # exit on error
 set -o errexit
 
+# Install system dependencies
+apt-get update
+apt-get install -y poppler-utils python3-opencv
+
+# Upgrade pip
+python -m pip install --upgrade pip
+
 # Install Python dependencies
-pip install -r requirements.txt
+pip install --no-cache-dir -r requirements.txt
 
 # Create required directories
 mkdir -p uploads
